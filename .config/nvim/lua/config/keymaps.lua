6e7fbe04849736389f82301cd73fbead57d9b894
ree2raz <inboxtorituraj@gmail.com>
2026-03-03 12:28:06 +0530

    fresh start

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- Remove LazyVim's default Ctrl+/ terminal binding
+vim.keymap.del("n", "<C-/>")
+
+-- Set your comment bindings
+vim.keymap.set("n", "<C-/>", "gcc", { remap = true, desc = "Toggle comment" })
+vim.keymap.set("v", "<C-/>", "gc", { remap = true, desc = "Toggle comment" })
